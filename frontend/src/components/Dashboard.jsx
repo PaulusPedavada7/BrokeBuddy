8a22ab45dcd158ef9922c29e42c8dd9c8fdcca39
Arya Bhavsar <aryab2914@gmail.com>
2025-12-11 12:43:45 -0500

    Added dark mode to the dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -3,11 +3,12 @@ import api from "../axios.jsx";
 import { UserContext } from "../App.jsx";
 
 function Dashboard() {
-  const [darkMode, setDarkMode] = useState(false);
   const { currentUser } = useContext(UserContext);
 
   return (
-    <p>Hello, {currentUser.first_name} {currentUser.last_name}</p>
+    <div className="min-h-screen flex bg-white dark:bg-gray-900">
+      <p className="dark:text-gray-100">Hello, {currentUser.first_name} {currentUser.last_name}</p>
+    </div>
   );
 }
 
