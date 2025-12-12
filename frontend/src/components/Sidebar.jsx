6bbfa64b1bc1b648dbb2ab96a811cf10af063e16
Arya Bhavsar <aryab2914@gmail.com>
2025-12-12 17:22:14 -0500

    Fixed css issues for the sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,16 +2,15 @@ import React, { useState , useContext } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { UserContext } from '../App.jsx';
 import ThemeToggle from './ThemeToggle';
-import { Bars3Icon, XMarkIcon, UserCircleIcon } from '@heroicons/react/24/solid';
+import { Bars3Icon, XMarkIcon, UserCircleIcon, UserIcon, HomeIcon } from '@heroicons/react/24/solid';
 
 export default function Sidebar() {
     const [isOpen, setIsOpen] = useState(true);
     const { currentUser } = useContext(UserContext);
+    const location = useLocation();
 
     // Function to check if the current path matches the link's path
-    const isActive = (path) => useLocation().pathname === path;
-    console.log(isActive("/dashboard"));
-    console.log(isActive("/profile"));
+    const isActive = (path) => location.pathname === path;
 
     // NOTE: Used backtick for className not single quotes to allow JS template literal and string interpolation
     return (
@@ -20,21 +19,22 @@ export default function Sidebar() {
             <div>
                 {/* Title and menu icon */}
                 <div className="flex items-center justify-between w-full p-4">
-                    {isOpen && <h2 className="text-xl font-bold px-3 py-2">BrokeBuddy</h2>}
+                    {isOpen && <h2 className="text-xl font-bold px-3">BrokeBuddy</h2>}
                     <button onClick={() => setIsOpen(!isOpen)} className="flex items-center justify-center w-10 h-10 text-gray-600 dark:text-gray-400">
                         {isOpen ? <XMarkIcon className="w-6 h-6" /> : <Bars3Icon className="w-6 h-6" />}
                     </button>
                 </div>
 
                 {/* Sidebar navigation links */}
-                <nav className="flex flex-col gap-1 mt-4 p-4">
+                <nav className={`flex flex-col gap-1 mt-4 ${isOpen ? "p-4": "px-2 py-4"}`}>
                     {/* CHANGE THE PLACEHOLDER LINKS (#) and use <Link to?> tag instead of <a> */}
                     {/* Make a list for the pages with name and links */}
-                    {/* NOTE: Used backtick for className not single quotes to allow JS template literal and string interpolation */}
-                    <a href="#" className={`flex gap-3 rounded-md px-3 py-2 ${isActive("/dashboard") ? 'text-blue-500 dark:text-white bg-black/5 dark:bg-white/5' : 'hover:text-blue-500 dark:hover:text-white hover:bg-black/5 dark:hover:bg-white/5'}`}>
+                    <Link to="/dashboard" className={`flex items-center gap-3 rounded-md px-3 py-2 ${isOpen && isActive("/dashboard") ? "text-blue-500 dark:text-white bg-black/5 dark:bg-white/5" : isOpen && "dark:text-gray-400 hover:text-blue-500 dark:hover:text-white hover:bg-black/5 dark:hover:bg-white/5"}`}>
+                        <HomeIcon className="w-6 h-6 shrink-0" />
                         {isOpen && <span>Dashboard</span>}
-                    </a>
-                    <a href="#" className={`flex gap-3 rounded-md px-3 py-2 ${isActive("/profile") ? 'text-blue-500 dark:text-white bg-black/5 dark:bg-white/5' : 'hover:text-blue-500 dark:hover:text-white hover:bg-black/5 dark:hover:bg-white/5'}`}>
+                    </Link>
+                    <a href="#" className={`flex items-center gap-3 rounded-md px-3 py-2 ${isOpen && isActive("/profile") ? "text-blue-500 dark:text-white bg-black/5 dark:bg-white/5" : isOpen && "dark:text-gray-400 hover:text-blue-500 dark:hover:text-white hover:bg-black/5 dark:hover:bg-white/5"}`}>
+                        <UserIcon className="w-6 h-6 shrink-0" />
                         {isOpen && <span>Profile</span>}
                     </a>
                 </nav>
