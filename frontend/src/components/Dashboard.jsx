6098179b82d116c09c1932674d8769be1a611a66
Arya Bhavsar <aryab2914@gmail.com>
2025-12-12 02:02:01 -0500

    Removed toggle from outside sidebar

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -12,7 +12,6 @@ function Dashboard() {
       <Sidebar />
       <div className="flex p-4">
         <p className="text-xl dark:text-gray-100">Hello, {currentUser.first_name} {currentUser.last_name}</p>
-        <ThemeToggle />
       </div>
     </div>
   );
